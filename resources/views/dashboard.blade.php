5e85dc28d6c4acf3438c281e636fd87e729b8be6
shohei <iwata_shohei@icloud.com>
2024-07-28 20:33:46 +0900

    基本UIの作成

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,18 +5,15 @@
         </h2>
     </x-slot>
 
-    <!-- sidebar -->
-    @section('sidebar')
-    @include('components.sidebar')
-    @stop
-
-    <!-- post button -->
-    <div class="post-button">
-        <button onclick="location.href='{{ route('post.index') }}'" type="button" class="btn btn-primary">投稿する</button>
+    <div class="postsection m-4">
+        <!-- post button -->
+        <div class="post-button">
+            <button onclick="location.href='{{ route('post.index') }}'" type="button" class="btn btn-primary">投稿する</button>
+        </div>
+    
+        <!-- posts section -->
+        <x-PostSection :posts="$post_infos" />
     </div>
-
-    <!-- posts section -->
-    <x-PostSection :posts="$post_infos" />
 
 
 </x-app-layout>
