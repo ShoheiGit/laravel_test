e5982eaaff490d078f79a4303c001708188875b9
shohei <iwata_shohei@icloud.com>
2024-08-25 14:33:43 +0900

    change

--- resources/views/channel/show.blade.php
+++ resources/views/channel/show.blade.php
@@ -6,7 +6,6 @@
       </div>
     @endif
 
-
       <a href="{{ route('channel.edit', ['channel' => $channel->id]) }}">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
           <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
